08e5b9f3a3b62989da817334bbda5917e81a5193
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-15 19:31:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,13 +26,13 @@ end
   #   return nuarray
   # end
   
-#   def assign_rooms(array)
-#   newarray = []
-#   counter = 1
-#   array.each do |name|
-#     newarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
-#     counter += 1
-#   end
-#   return newarray
-# end
+  def assign_rooms(array)
+  newarray = []
+  counter = 1
+  array.each do |name|
+    newarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return newarray
+end
   
