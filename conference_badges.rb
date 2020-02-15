42a3f458a0092315db623806b53c004580969357
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-15 19:27:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,23 +16,23 @@ def batch_badge_creator(array)
   return newarray
 end
 
-#   def assign_rooms(array)
-#   newarray = []
-#   counter = 1
-#   array.each do |name|
-#     newarray.push("Hello, #{name}! You'll be assinged to room #{counter}!")
-#   counter += 1
-#   end
-#     return newarray
-#   end
-  
   def assign_rooms(array)
   newarray = []
   counter = 1
   array.each do |name|
-    newarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
-    counter += 1
+    newarray.push("Hello, #{name}! You'll be assinged to room #{counter}!")
+  counter += 1
   end
-  return newarray
-end
+    return newarray
+  end
+  
+#   def assign_rooms(array)
+#   newarray = []
+#   counter = 1
+#   array.each do |name|
+#     newarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+#     counter += 1
+#   end
+#   return newarray
+# end
   
