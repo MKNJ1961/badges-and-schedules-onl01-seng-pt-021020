79d530a6eb4ec5cc6bfe4dfdd35c468ce21d4078
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-15 19:06:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,20 +1,4 @@
 # Write your code here.
-people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def badge_maker(name)
-  return "Hello, my name is #{name}."
-end
-
-
-
-def batch_badge_creator(array)
-  newarray = []
-  array.each do |name|
-    newarray.push("Hello, my name is #{name}.")
-end 
-  return newarray
-end
-  
-end
   
   
