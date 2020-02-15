eef2aff443caf652d214e23dd7719b7ef8c13345
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-15 19:21:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 
-people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+people = ["Edgar", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
   return "Hello, my name is #{name}."
